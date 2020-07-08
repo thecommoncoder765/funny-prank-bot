fad16d4494bc840161a5a4a526dba16f54d35207
Elijah Sears <elijahsears7@gmail.com>
2020-07-08 18:10:36 -0500

    Make "index.js" even better!  :)

--- src/main/index.js
+++ src/main/index.js
@@ -22,23 +22,12 @@ bot.on('message', msg => {
     let args = msg.content.substring(PREFIX.length).split(" ");
 
 
-    if (msg.content === "?") {
-        msg.reply('Ok.. I am listening!');
-    }
-
-    if (msg.content === "do you like Jimmy Neutron") {
-        msg.reply('YES! YES! YES! YES!  YES! YES! YES! YES! YES! YES! YES! YES! YES! YES! YES! YES! YES! YES! YES! YES! YES! YES!');
-    }
 
     switch (args[0]) {
         case 'act-like-Carl-Wheezer':
 
             if (!args[1]) {
 
-                if (args[1] === 'stop') {
-                    msg.reply('ok, sorry');
-                    break;
-                }
 
                 msg.reply('OKEY DOKEY!!!');
                 msg.reply('ARE YOU GOING TO FINISH THAT CWASOOOOOONTTTT!!!!!!?????!!!');
